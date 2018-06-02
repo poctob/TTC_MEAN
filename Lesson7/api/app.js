1c3cb663fd5ffe28545fa5bf7124ef7ae80e7f74
EC2 Default User <ec2-user@ip-172-31-74-171.ec2.internal>
2018-06-02 15:35:50 +0000

    Edits for lesson 7

--- Lesson7/api/app.js
+++ Lesson7/api/app.js
@@ -4,30 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const DB = require('./app/models/db');
-
-const db = new DB();
-db.connect();
-var apiRouter = require('./app/routes/api');
-
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('../heroes/dist/heroes'));
-app.use('/api', apiRouter);
+app.use(express.static(path.join('..', 'web', 'dist', 'web')));
 
-
-// app.get('*', function (req, res) {
-//     res.sendFile('../heroes/dist/heroes/index.html');
-// });
-
-app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: '../heroes/dist/heroes' });
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,7 +29,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.sendStatus(err.status || 500);
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
